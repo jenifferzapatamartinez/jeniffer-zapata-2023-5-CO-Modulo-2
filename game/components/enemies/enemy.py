ec6fe2a88c23bed1f11966a56efa91a2996281c0
jenifferzapatamartinez <Jeniffer.Zapata@fundacion-jala.org>
2023-06-14 17:29:55 -0500

    First-Class-Homework

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,20 +2,23 @@ import pygame
 import random
 
 from pygame.sprite import Sprite
-from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import ENEMY_1,ENEMY_2, SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 class Enemy(Sprite):
     ENEMY_WHIDTH = 40
     ENEMY_HEIGHT = 60
-    X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 1000]
+    X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500,
+                  550,600,650,700,750,800,850,900,950,1000,1050]
     Y_POS= 20
     SPEED_X = 5 
     SPEED_Y = 4
     MOV_X = { 0: 'left', 1: 'right' }
 
     def __init__(self):
-        self.image = pygame.transform.scale(ENEMY_1, (self.ENEMY_WHIDTH,self.ENEMY_HEIGHT))
+        self.image =pygame.transform.scale(ENEMY_1, (self.ENEMY_WHIDTH,self.ENEMY_HEIGHT))
+        self.enemy1=pygame.transform.scale(ENEMY_1, (self.ENEMY_WHIDTH,self.ENEMY_HEIGHT))
+        self.enemy2=pygame.transform.scale(ENEMY_2, ((self.ENEMY_WHIDTH),(self.ENEMY_HEIGHT)))
         self.rect = self.image.get_rect()
         self.rect.x = self.X_POS_LIST[random.randint(0 , 10)]
         self.rect.y = self.Y_POS
@@ -24,6 +27,7 @@ class Enemy(Sprite):
         self.movement_x = self.MOV_X[random.randint(0, 1)]
         self.move_x_for = random.randint(30, 100)
         self.index = 0
+        self.i=0
 
     def update(self, ships):
         self.rect.y += self.speed_y
@@ -38,9 +42,10 @@ class Enemy(Sprite):
         if self.rect.y >= SCREEN_HEIGHT:
             ships.remove(self)
 
+
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
-
+            
     def change_movement_x(self):
         self.index += 1
         if (self.index >= self.move_x_for and self.movement_x == 'right' or (self.rect.x >= SCREEN_WIDTH)):
@@ -49,3 +54,10 @@ class Enemy(Sprite):
         elif (self.index >= self.move_x_for and self.movement_x == 'left' or (self.rect.x <= 10)):
             self.movement_x = 'right'
             self.index = 0
+
+    def multy_enemy(self,enemy):
+        if enemy == 1:
+            self.image=self.enemy1
+        else:
+            self.image=self.enemy2
+            
--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -4,6 +4,7 @@ from game.components.enemies.enemy import Enemy
 class EnemyManager:
     def __init__(self):
         self.enemies = []
+        self.index=0
 
     def update(self):
         self.add_enemy()
@@ -12,11 +13,21 @@ class EnemyManager:
             enemy.update(self.enemies)
 
     def draw(self, screen):
-        for enemy in self.enemies:
-            enemy.draw(screen)
+        for enemy in self.enemies: 
+            enemy.draw(screen)            
 
     def add_enemy(self):
+        self.index +=1
         if len(self.enemies) < 1:
             enemy = Enemy()
+            enemy.multy_enemy(self.index)
+            if self.index == 2:
+                self.index=0
             self.enemies.append(enemy)
+        #Para dos enemigos
+        #elif len(self.enemies) == 1:
+         #   enemy2 = Enemy(self.index)
+         #   enemy2.multy_enemy(2)
+         #   self.enemies.append(enemy2)
+
   
